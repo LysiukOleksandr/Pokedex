1cbef3c14d078fa6fcbecc29d3d18c527cf5133d
unknown <olysiuk01@gmail.com>
2020-11-22 15:24:18 +0200

    PokemonDetails + react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import './App.css';
 import Header from './components/Header/Header'
 import Home from './pages/Home'
-// import Test from './components/Test'
-
+import PokemonDetails from './pages/PokemonDetails/PokemonDetails'
+import { Route, BrowserRouter } from "react-router-dom";
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <header>
         <Header/>
       </header>
       <main>
-        <Home/>
-        {/* <Test /> */}
+        {/* <Route exact path='/' component={Home} />
+        <Route path='/:name' component={PokemonDetails} /> */}
+        <PokemonDetails/>
       </main>
     </div>
+    </BrowserRouter>
   );
 }
 
--- src/pages/PokemonDetails/PokemonDetails.jsx
+++ src/pages/PokemonDetails/PokemonDetails.jsx
@@ -1,10 +1,20 @@
-import react from "React"
+import React from "react"
 import './PokemonDetails.css'
 
 const PokemonDetails = () =>{
-  
+  return(
+    <div className="pokemon-details">
+      <img className='pokemon-details__image' src ='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'  />
+      <p className="pokemon-details__id">1</p>
+      <h1 className='pokemon-details__title'>Name: Bulbasaur</h1>
+      <p className="pokemon-details__weight">Weight: 56</p>
+      <p className="pokemon-details__experience">Experience: 64</p>
+      <p className="pokemon-details__hp">Experience: 64</p>
+      <p className="pokemon-details__defense">Defense: 49</p>
+      <p className="pokemon-details__attack">Attack: 51</p>
+    </div>
+  )
 
-  <div>dt</div>
 
 
 }
